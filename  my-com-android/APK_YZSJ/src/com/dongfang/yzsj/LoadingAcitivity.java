75c27c6e89a5a6cae75bd4dec77de3383d346aa2
dongfanglinglong <dongfanglinglong@gmail.com>
2013-09-11 05:36:51 +0000

    晚上个人中心订购页面

---  my-com-android/APK_YZSJ/src/com/dongfang/yzsj/LoadingAcitivity.java
+++  my-com-android/APK_YZSJ/src/com/dongfang/yzsj/LoadingAcitivity.java
@@ -2,10 +2,13 @@ package com.dongfang.yzsj;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.TextUtils;
+import android.widget.Toast;
 
 import com.df.util.ULog;
 import com.dongfang.utils.ACache;
+import com.dongfang.utils.Util;
 import com.dongfang.yzsj.bean.HomeBean;
 import com.dongfang.yzsj.params.ComParams;
 import com.lidroid.xutils.HttpUtils;
@@ -32,7 +35,6 @@ public class LoadingAcitivity extends BaseActivity {
 		setContentView(R.layout.activity_loading);
 		// User.saveUserLoginStatu(this, false);
 
-		
 	}
 
 	/** 跳转到MainActivity */
@@ -54,7 +56,22 @@ public class LoadingAcitivity extends BaseActivity {
 	@Override
 	protected void onResume() {
 		super.onResume();
+		if (Util.isNetworkAvailable(this)) {
+			new Handler().postAtTime(new Runnable() {
 
+				@Override
+				public void run() {
+					initData();
+				}
+			}, 100);
+		}
+		else {
+			Toast.makeText(this, "请检查网络连接", Toast.LENGTH_LONG).show();
+		}
+
+	}
+
+	private void initData() {
 		// 需要判断网络
 		HomeBean bean = new com.google.gson.Gson().fromJson(ACache.get(this).getAsString(ComParams.INTENT_HOMEBEAN),
 				HomeBean.class);
@@ -74,9 +91,8 @@ public class LoadingAcitivity extends BaseActivity {
 					HomeBean bean = new com.google.gson.Gson().fromJson(result, HomeBean.class);
 					bean.toLog();
 					// 缓存数据
-					ACache.get(LoadingAcitivity.this).put(ComParams.INTENT_HOMEBEAN, result, ACache.TIME_DAY * 7);
+					ACache.get(LoadingAcitivity.this).put(ComParams.INTENT_HOMEBEAN, result, ACache.TIME_HOUR);
 					// ACache.get(LoadingAcitivity.this).put(ComParams.INTENT_HOMEBEAN, result, 60 * 5);
-
 					intent(bean);
 
 				}
