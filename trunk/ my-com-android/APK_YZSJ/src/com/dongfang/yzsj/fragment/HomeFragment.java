cc3f6cb3bcf4beebb031c5e85589dcbe3ab7694c
dongfanglinglong@gmail.com <dongfanglinglong@gmail.com@afd60443-85ab-dd9c-788a-5eb6f25d9bf9>
2013-10-10 07:37:19 +0000

    增加本版到1.0.0.5； 修改首页vip图片，隐藏vip更多按钮； 修复播放记录和个人搜藏适配器和对应布局； 修复直播登录时崩溃问题； 修改详情页剧集显示，使得每个剧集按钮显示大小一致；

--- trunk/ my-com-android/APK_YZSJ/src/com/dongfang/yzsj/fragment/HomeFragment.java
+++ trunk/ my-com-android/APK_YZSJ/src/com/dongfang/yzsj/fragment/HomeFragment.java
@@ -4,7 +4,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTabHost;
