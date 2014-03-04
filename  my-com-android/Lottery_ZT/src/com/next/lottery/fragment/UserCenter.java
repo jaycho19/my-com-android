ef2bd6525a0dd3bdbad2d2ebb18dfc00796f5bb5
dongfanglinglong <dongfanglinglong@gmail.com>
2014-03-04 12:18:31 +0000

    增加登录页面

---
+++  my-com-android/Lottery_ZT/src/com/next/lottery/fragment/UserCenter.java
@@ -0,0 +1,7 @@
+package com.next.lottery.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class UserCenter extends Fragment {
+
+}
---
+++  my-com-android/Lottery_ZT/src/com/next/lottery/view/SlipButton.java
@@ -0,0 +1,233 @@
+package com.next.lottery.view;
+
+import android.annotation.SuppressLint;
+import android.app.ActionBar.LayoutParams;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.text.Layout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+
+import com.lidroid.xutils.util.LogUtils;
+import com.next.lottery.R;
+
+/**
+ * 仿IOS滑动按钮，用OnChangedListener接口来处理按钮状态更变
+ * 
+ * @author dongfang
+ * 
+ */
+@SuppressLint("Recycle")
+public class SlipButton extends View implements OnTouchListener, OnClickListener {
+
+	private boolean NowChoose = false; // 记录当前按钮是否打开,true为打开,flase为关闭
+
+	private boolean isChecked;
+	private boolean isChgLsnOn = false;
+	private boolean OnSlip = false; // 记录用户是否在滑动的变量
+	private float DownX, NowX; // 按下时的x,当前的x
+	private Rect Btn_On, Btn_Off; // 打开和关闭状态下,游标的Rect .
+	private OnChangedListener ChgLsn;
+	private Bitmap bg_on, bg_off, slip_btn;
+	private boolean isClick = false;
+	private Drawable bgon = null, bgoff = null, slipbtn = null;
+
+	public SlipButton(Context context) {
+		super(context);
+	}
+
+	public SlipButton(Context context, AttributeSet attrs) {
+
+		super(context, attrs);
+		TypedArray typeArray = context.obtainStyledAttributes(attrs, R.styleable.silpinfo);
+		bgon = typeArray.getDrawable(R.styleable.silpinfo_bgon);
+		bgoff = typeArray.getDrawable(R.styleable.silpinfo_bgoff);
+		slipbtn = typeArray.getDrawable(R.styleable.silpinfo_slipbtn);
+		init(bgon, bgoff, slipbtn);
+	}
+
+	public SlipButton(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		TypedArray typeArray = context.obtainStyledAttributes(attrs, R.styleable.silpinfo);
+		bgon = typeArray.getDrawable(R.styleable.silpinfo_bgon);
+		bgoff = typeArray.getDrawable(R.styleable.silpinfo_bgoff);
+		slipbtn = typeArray.getDrawable(R.styleable.silpinfo_slipbtn);
+		init(bgon, bgoff, slipbtn);
+	}
+
+	private void init(Drawable bgon, Drawable bgoff, Drawable slipbtn) {// 初始化
+		BitmapDrawable mBgon = (BitmapDrawable) bgon;
+		BitmapDrawable mBgoff = (BitmapDrawable) bgoff;
+		BitmapDrawable mSlipbtn = (BitmapDrawable) slipbtn;
+
+		bg_on = mBgon.getBitmap(); // BitmapFactory.decodeResource(getResources(), R.drawable.icon3_selected);
+		bg_off = mBgoff.getBitmap();// BitmapFactory.decodeResource(getResources(), R.drawable.icon3);
+		slip_btn = mSlipbtn.getBitmap(); // BitmapFactory.decodeResource(getResources(), R.drawable.icon4);
+
+		LogUtils.d(slipbtn.getIntrinsicWidth() + "---------------" + slip_btn.getWidth());
+		Btn_On = new Rect(0, 0, slip_btn.getWidth(), slip_btn.getHeight());
+
+		Btn_Off = new Rect(bg_off.getWidth() - slip_btn.getWidth(), 0, bg_off.getWidth(), slip_btn.getHeight());
+		setOnTouchListener(this);// 设置监听器,也可以直接复写OnTouchEvent
+		//setOnClickListener(this);// 设置点击的监听
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {// 绘图函数
+
+		super.onDraw(canvas);
+
+		Matrix matrix = new Matrix();
+		Paint paint = new Paint();
+		float x;
+
+		if (NowX < (bg_on.getWidth() / 2))// 滑动到前半段与后半段的背景不同,在此做判断
+		{
+			x = NowX - slip_btn.getWidth() / 2;
+			canvas.drawBitmap(bg_off, matrix, paint);// 画出关闭时的背景
+		}
+
+		else {
+			x = bg_on.getWidth() - slip_btn.getWidth() / 2;
+			canvas.drawBitmap(bg_on, matrix, paint);// 画出打开时的背景
+		}
+
+		if (OnSlip)// 是否是在滑动状态,
+		{
+			if (NowX >= bg_on.getWidth())// 是否划出指定范围,不能让游标跑到外头,必须做这个判断
+
+				x = bg_on.getWidth() - slip_btn.getWidth() / 2;// 减去游标1/2的长度...
+
+			else if (NowX < 0) {
+				x = 0;
+			}
+			else {
+				x = NowX - slip_btn.getWidth() / 2;
+			}
+		}
+		else if (!isClick) {// 非滑动状态
+			if (NowChoose) {// 根据现在的开关状态设置画游标的位置
+				x = Btn_Off.left;
+				canvas.drawBitmap(bg_on, matrix, paint);// 初始状态为true时应该画出打开状态图片
+			}
+			else {
+				x = Btn_On.left;
+				canvas.drawBitmap(bg_off, matrix, paint);// 初始状态为true时应该画出打开状态图片
+			}
+		}
+		if (isClick) {
+			if (NowChoose) {// 根据现在的开关状态设置画游标的位置
+				x = Btn_Off.left;
+				canvas.drawBitmap(bg_on, matrix, paint);// 初始状态为true时应该画出打开状态图片
+			}
+			else {
+				x = Btn_On.left;
+				canvas.drawBitmap(bg_off, matrix, paint);// 初始状态为true时应该画出打开状态图片
+			}
+		}
+		// if (isChecked) {
+		// canvas.drawBitmap(bg_on, matrix, paint);
+		// x = Btn_Off.left;
+		// isChecked = !isChecked;
+		// }
+
+		if (x < 0) {// 对游标位置进行异常判断...
+			x = 0;
+		}
+		else if (x > bg_on.getWidth() - slip_btn.getWidth()) {
+			x = bg_on.getWidth() - slip_btn.getWidth();
+		}
+		canvas.drawBitmap(slip_btn, x, 0, paint);// 画出游标.
+
+	}
+
+	public boolean onTouch(View v, MotionEvent event) {
+		switch (event.getAction())
+		// 根据动作来执行代码
+
+		{
+		case MotionEvent.ACTION_MOVE:// 滑动
+			NowX = event.getX();
+			break;
+
+		case MotionEvent.ACTION_DOWN:// 按下
+
+			if (event.getX() > bg_on.getWidth() || event.getY() > bg_on.getHeight())
+				return false;
+			OnSlip = true;
+			DownX = event.getX();
+			NowX = DownX;
+			break;
+
+		case MotionEvent.ACTION_CANCEL: // 移到控件外部
+
+			OnSlip = false;
+			boolean choose = NowChoose;
+			if (NowX >= (bg_on.getWidth() / 2)) {
+				NowX = bg_on.getWidth() - slip_btn.getWidth() / 2;
+				NowChoose = true;
+			}
+			else {
+				NowX = NowX - slip_btn.getWidth() / 2;
+				NowChoose = false;
+			}
+			if (isChgLsnOn && (choose != NowChoose)) // 如果设置了监听器,就调用其方法..
+				ChgLsn.OnChanged(NowChoose);
+			break;
+		case MotionEvent.ACTION_UP:// 松开
+			OnSlip = false;
+			boolean LastChoose = NowChoose;
+			if (event.getX() >= (bg_on.getWidth() / 2)) {
+				NowX = bg_on.getWidth() - slip_btn.getWidth() / 2;
+				NowChoose = true;
+			}
+			else {
+				NowX = NowX - slip_btn.getWidth() / 2;
+				NowChoose = false;
+			}
+			if (isChgLsnOn && (LastChoose != NowChoose)) { // 如果设置了监听器,就调用其方法..
+				ChgLsn.OnChanged(NowChoose);
+			}
+			break;
+		default:
+		}
+		invalidate();// 重画控件
+		return true;
+	}
+
+	public void SetOnChangedListener(OnChangedListener l) {// 设置监听器,当状态修改的时候
+		isChgLsnOn = true;
+		ChgLsn = l;
+	}
+
+	public interface OnChangedListener {
+		abstract void OnChanged(boolean CheckState);
+	}
+
+	public void setCheck(boolean isChecked) {
+		this.isChecked = isChecked;
+		NowChoose = isChecked;
+	}
+
+	@Override
+	public void onClick(View v) {
+		isClick = true;
+		invalidate();
+		if (NowChoose) {
+			NowChoose = false;
+		}
+		else {
+			NowChoose = true;
+		}
+	}
+}
