5875e810f792297d53e8f37c9d4f6322009b9a03
dongfanglinglong <dongfanglinglong@gmail.com>
2014-03-10 04:03:35 +0000

    增加二维码扫描功能

---  my-com-android/Lottery_ZT/src/com/next/lottery/fragment/HomeFragment.java
+++  my-com-android/Lottery_ZT/src/com/next/lottery/fragment/HomeFragment.java
@@ -1,11 +1,15 @@
 package com.next.lottery.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.next.lottery.MCaptureActivity;
 import com.next.lottery.R;
 
 public class HomeFragment extends Fragment {
@@ -13,6 +17,18 @@ public class HomeFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_home, container, false);
 		// initView(view);
+
+		view.findViewById(R.id.app_top_title_iv_left).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setClass(getActivity(), MCaptureActivity.class);
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivityForResult(intent, 1);
+			}
+		});
+
 		return view;
 	}
 }
