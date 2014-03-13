bbc9d02fc9c34bcd04539d947210cad0893c0437
dongfanglinglong@gmail.com <dongfanglinglong@gmail.com@afd60443-85ab-dd9c-788a-5eb6f25d9bf9>
2014-03-13 03:12:59 +0000

    增加UserInfoActivity页面

--- trunk/ my-com-android/Lottery_ZT/src/com/next/lottery/fragment/UserCenterFragment.java
+++ trunk/ my-com-android/Lottery_ZT/src/com/next/lottery/fragment/UserCenterFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.dongfang.utils.ULog;
 import com.dongfang.v4.app.BaseFragment;
@@ -15,6 +16,7 @@ import com.lidroid.xutils.view.annotation.event.OnClick;
 import com.next.lottery.LRLoginActivity;
 import com.next.lottery.R;
 import com.next.lottery.SettingActivity;
+import com.next.lottery.UserInfoActivity;
 import com.next.lottery.utils.User;
 
 /**
@@ -27,6 +29,13 @@ public class UserCenterFragment extends BaseFragment {
 
 	@ViewInject(R.id.app_top_title_iv_rigth)
 	private ImageView ivSetting;
+	@ViewInject(R.id.usercenter_iv_user_head)
+	private ImageView ivHead;
+
+	@ViewInject(R.id.usercenter_tv_user_name)
+	private TextView tvUserName;
+	@ViewInject(R.id.usercenter_tv_user_tishenghuiyuan)
+	private TextView tvTiShengHuiYuan;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -34,25 +43,27 @@ public class UserCenterFragment extends BaseFragment {
 			startActivity(new Intent(getActivity(), LRLoginActivity.class));
 			return null;
 		}
-
 		View view = inflater.inflate(R.layout.fragment_usercenter, container, false);
-		
-		ULog.e("000000000000000000000 ivSetting == null " + (null == ivSetting));
-		ViewUtils.inject(this,view);
-		ULog.e("ivSetting == null " + (null == ivSetting));
-		ivSetting = (ImageView) view.findViewById(R.id.app_top_title_iv_rigth);
-		ULog.e("ivSetting == null " + (null == ivSetting));
+		ViewUtils.inject(this, view);
 		
 		return view;
 	}
 
 	@Override
-	@OnClick({ R.id.app_top_title_iv_rigth })
+	@OnClick({ R.id.app_top_title_iv_rigth, R.id.usercenter_iv_user_head, R.id.usercenter_tv_user_name,
+			R.id.usercenter_tv_user_tishenghuiyuan
+
+	})
 	public void onClick(View v) {
 		ULog.d("id = " + v.getId());
 		switch (v.getId()) {
 		case R.id.app_top_title_iv_rigth:
 			startActivity(new Intent(getActivity(), SettingActivity.class));
+			break;
+		case R.id.usercenter_iv_user_head:
+		case R.id.usercenter_tv_user_name:
+		case R.id.usercenter_tv_user_tishenghuiyuan:
+			startActivity(new Intent(getActivity(), UserInfoActivity.class));
 			break;
 		default:
 			break;
