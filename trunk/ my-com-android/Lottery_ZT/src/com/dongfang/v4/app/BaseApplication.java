d2e5f93e6ffb33c3226a0970ed559f0de187c7ba
dongfanglinglong@gmail.com <dongfanglinglong@gmail.com@afd60443-85ab-dd9c-788a-5eb6f25d9bf9>
2014-03-18 06:31:49 +0000

    完善购物车的sku页面

--- trunk/ my-com-android/Lottery_ZT/src/com/dongfang/v4/app/BaseApplication.java
+++ trunk/ my-com-android/Lottery_ZT/src/com/dongfang/v4/app/BaseApplication.java
@@ -1,7 +1,12 @@
 package com.dongfang.v4.app;
 
 import android.app.Application;
+import android.content.Context;
 import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+import com.dongfang.utils.ULog;
 
 /** @author dongfang */
 public class BaseApplication extends Application {
@@ -66,17 +71,10 @@ public class BaseApplication extends Application {
 
 	public void onCreate() {
 		super.onCreate();
-		// QAS.appPrepare(this, TAG);
-		// try {
-		// ULog.isDug = Boolean.valueOf(getResources().getString(R.string.system_debug_on));
-		//
-		// } catch (Exception e) {
-		// ULog.isDug = false;
-		// }
-		// ULog.i("BLABLA", "BASE APP-->" + Thread.currentThread().getId());
+		ULog.i("BASE APP-->" + Thread.currentThread().getId());
+		getDeviceInfo();
 		// myApplication = this;
 		// actionList = new ArrayList<ActionReport>();
-		// getDeviceInfo();
 		//
 		// /* 上报异常错误功能，禁止关闭 */
 		// // debug模式关闭
@@ -85,31 +83,31 @@ public class BaseApplication extends Application {
 		// }
 	}
 
-	public void getDeviceInfo() {
-		// DisplayMetrics dm = new DisplayMetrics();
-		// WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-		// wm.getDefaultDisplay().getMetrics(dm);
+	private void getDeviceInfo() {
+		DisplayMetrics dm = new DisplayMetrics();
+		WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+		wm.getDefaultDisplay().getMetrics(dm);
 
-		// if (dm.widthPixels < dm.heightPixels) {
-		// // 竖屏
-		// Constants.SCREEN_WIDTH_PORTRAIT = dm.widthPixels;
-		// Constants.SCREEN_HEIGHT_PORTRAIT = dm.heightPixels;
-		//
-		// Constants.SCREEN_HEIGHT_LANDSCAPE = dm.widthPixels;
-		// Constants.SCREEN_WIDTH_LANDSCAPE = dm.heightPixels;
-		// }
-		// else {
-		// // 横屏
-		// Constants.SCREEN_WIDTH_PORTRAIT = dm.heightPixels;
-		// Constants.SCREEN_HEIGHT_PORTRAIT = dm.widthPixels;
-		//
-		// Constants.SCREEN_HEIGHT_LANDSCAPE = dm.heightPixels;
-		// Constants.SCREEN_WIDTH_LANDSCAPE = dm.widthPixels;
-		// }
-		// Constants.DENSITYDPI = dm.densityDpi;
-		// Constants.DENSITY = dm.density;
-		// ULog.i(TAG, "setupBaseData, SCREEN_WIDTH = " + dm.widthPixels + ", SCREEN_HEIGHT = " + dm.heightPixels
-		// + ", densityDpi = " + dm.densityDpi);
+		if (dm.widthPixels < dm.heightPixels) {
+			// 竖屏
+			DeviceInfo.SCREEN_WIDTH_PORTRAIT = dm.widthPixels;
+			DeviceInfo.SCREEN_HEIGHT_PORTRAIT = dm.heightPixels;
+
+			DeviceInfo.SCREEN_HEIGHT_LANDSCAPE = dm.widthPixels;
+			DeviceInfo.SCREEN_WIDTH_LANDSCAPE = dm.heightPixels;
+		}
+		else {
+			// 横屏
+			DeviceInfo.SCREEN_WIDTH_PORTRAIT = dm.heightPixels;
+			DeviceInfo.SCREEN_HEIGHT_PORTRAIT = dm.widthPixels;
+
+			DeviceInfo.SCREEN_HEIGHT_LANDSCAPE = dm.heightPixels;
+			DeviceInfo.SCREEN_WIDTH_LANDSCAPE = dm.widthPixels;
+		}
+		DeviceInfo.DENSITYDPI = dm.densityDpi;
+		DeviceInfo.DENSITY = dm.density;
+		ULog.d("setupBaseData, SCREEN_WIDTH = " + dm.widthPixels + ", SCREEN_HEIGHT = " + dm.heightPixels
+				+ ", densityDpi = " + dm.densityDpi);
 	}
 
 }
---
+++ trunk/ my-com-android/Lottery_ZT/src/com/dongfang/views/ScrollViewExtend.java
@@ -0,0 +1,69 @@
+package com.dongfang.views;
+
+import com.dongfang.v4.app.DeviceInfo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.LinearLayout.LayoutParams;
+
+/**
+ * 能够兼容ViewPager的ScrollView
+ * 
+ * @Description: 解决了ViewPager在ScrollView中的滑动反弹问题
+ * @author dongfang
+ */
+public class ScrollViewExtend extends ScrollView {
+	// 滑动距离及坐标
+	private float xDistance, yDistance, xLast, yLast;
+
+	private boolean allow_match = true;
+
+	public boolean isAllow_match() {
+		return allow_match;
+	}
+
+	public void setAllow_match(boolean allow_match) {
+		this.allow_match = allow_match;
+	}
+
+	public ScrollViewExtend(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		// TODO Auto-generated method stub
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		if (!allow_match && (getMeasuredHeight() * 2 > DeviceInfo.SCREEN_HEIGHT_PORTRAIT)) {
+			setMeasuredDimension(getMeasuredWidth(), DeviceInfo.SCREEN_HEIGHT_PORTRAIT / 2);
+		}
+	}
+
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		switch (ev.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			xDistance = yDistance = 0f;
+			xLast = ev.getX();
+			yLast = ev.getY();
+			break;
+		case MotionEvent.ACTION_MOVE:
+			final float curX = ev.getX();
+			final float curY = ev.getY();
+
+			xDistance += Math.abs(curX - xLast);
+			yDistance += Math.abs(curY - yLast);
+			xLast = curX;
+			yLast = curY;
+
+			if (xDistance > yDistance) {
+				return false;
+			}
+		}
+
+		return super.onInterceptTouchEvent(ev);
+	}
+}
