8bbf25478b79f53b6634aa481e461c95ac404606
dongfanglinglong@gmail.com <dongfanglinglong@gmail.com@afd60443-85ab-dd9c-788a-5eb6f25d9bf9>
2014-03-28 08:36:14 +0000

    用户信息，帮助中心，设置 增加右侧菜单

--- trunk/ my-com-android/Lottery_ZT/src/com/next/lottery/fragment/ClassifyFragment.java
+++ trunk/ my-com-android/Lottery_ZT/src/com/next/lottery/fragment/ClassifyFragment.java
@@ -31,10 +31,8 @@ import com.next.lottery.fragment.adapter.ClassifyRightListViewAdapter;
  * 
  */
 public class ClassifyFragment extends BaseFragment {
-	private String[] recipes2 = { "毛衣", "上衣", "衬衫", "连衣裙", "毛衣", "上衣", "衬衫",
-			"连衣裙", "毛衣", "上衣", "衬衫", "连衣裙" };
-	private String[] Lefttitles = { "热销商品", "经典系列", "男士系列", "女士系列", "儿童系列",
-			"箱包系列", "配件 礼品", "家居系列", "汽车用品", "母婴" };
+	private String[] recipes2 = { "毛衣", "上衣", "衬衫", "连衣裙", "毛衣", "上衣", "衬衫", "连衣裙", "毛衣", "上衣", "衬衫", "连衣裙" };
+	private String[] Lefttitles = { "热销商品", "经典系列", "男士系列", "女士系列", "儿童系列", "箱包系列", "配件 礼品", "家居系列", "汽车用品", "母婴" };
 
 	@ViewInject(R.id.listview_content_left)
 	private ListView listViewleft;
@@ -42,17 +40,15 @@ public class ClassifyFragment extends BaseFragment {
 	private ListView listViewRight;
 	@ViewInject(R.id.layout_content_right)
 	private LinearLayout lin_content_right;
-	
+
 	private ClassifyLeftListViewAdapter leftAdapter;
-	
-	private float oldX=0;//记录滑动时横坐标
-	private float oldY=0;//记录滑动时竖坐标
-	
+
+	private float oldX = 0;// 记录滑动时横坐标
+	private float oldY = 0;// 记录滑动时竖坐标
+
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_classify, container,
-				false);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_classify, container, false);
 		ViewUtils.inject(this, view);
 		initLeftListView();
 		setListener();
@@ -60,22 +56,19 @@ public class ClassifyFragment extends BaseFragment {
 	}
 
 	private void initLeftListView() {
-		leftAdapter = new ClassifyLeftListViewAdapter(
-				getActivity(), R.layout.fragment_classify_left, Lefttitles);
+		leftAdapter = new ClassifyLeftListViewAdapter(getActivity(), R.layout.fragment_classify_left, Lefttitles);
 		listViewleft.setAdapter(leftAdapter);
 	}
 
 	private void setListener() {
 		listViewleft.setOnItemClickListener(new OnItemClickListener() {
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 				lin_content_right.setVisibility(View.VISIBLE);
-				leftAdapter.setIsRightShowAndPosition(true,arg2);
+				leftAdapter.setIsRightShowAndPosition(true, arg2);
 				leftAdapter.notifyDataSetChanged();
-				ClassifyRightListViewAdapter adapter = new ClassifyRightListViewAdapter(
-						getActivity(), R.layout.fragment_classify_right_item,
-						recipes2);
+				ClassifyRightListViewAdapter adapter = new ClassifyRightListViewAdapter(getActivity(),
+						R.layout.fragment_classify_right_item, recipes2);
 				listViewRight.setAdapter(adapter);
 
 			}
@@ -84,48 +77,47 @@ public class ClassifyFragment extends BaseFragment {
 		listViewRight.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 
 			}
 		});
-		
+
 		listViewRight.setOnTouchListener(new OnTouchListener() {
-			
+
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
 				// TODO Auto-generated method stub
-				
-				ULog.i("event-->"+event.getAction());
-				
-					switch (event.getAction()) {
-					case MotionEvent.ACTION_DOWN:
-						ULog.i("起始x坐标-->"+event.getX());
-						oldX = event.getX();
-						oldY = event.getY();
-						break;
-					case MotionEvent.ACTION_CANCEL:
-						oldX = 0F;
-						oldY = 0F;
-						break;
-					case MotionEvent.ACTION_MOVE:
-						ULog.i("正在活动的x坐标-->"+event.getX());
-						break;
-					case MotionEvent.ACTION_UP:
-						ULog.i("结束x坐标-->"+event.getX());
-						if (oldX>0&&event.getX()-oldX>120&&event.getY()-oldY<200) {
-							ULog.i("左划，消失右边Listview");
-							lin_content_right.setVisibility(View.GONE);
-							leftAdapter.setIsRightShowAndPosition(false,-1);
-							leftAdapter.notifyDataSetChanged();
-						}
-						oldX = 0F;
-						oldY = 0F;
-						break;
 
-					default:
-						break;
+				ULog.i("event-->" + event.getAction());
+
+				switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					ULog.i("起始x坐标-->" + event.getX());
+					oldX = event.getX();
+					oldY = event.getY();
+					break;
+				case MotionEvent.ACTION_CANCEL:
+					oldX = 0F;
+					oldY = 0F;
+					break;
+				case MotionEvent.ACTION_MOVE:
+					ULog.i("正在活动的x坐标-->" + event.getX());
+					break;
+				case MotionEvent.ACTION_UP:
+					ULog.i("结束x坐标-->" + event.getX());
+					if (oldX > 0 && event.getX() - oldX > 120 && event.getY() - oldY < 200) {
+						ULog.i("左划，消失右边Listview");
+						lin_content_right.setVisibility(View.GONE);
+						leftAdapter.setIsRightShowAndPosition(false, -1);
+						leftAdapter.notifyDataSetChanged();
 					}
+					oldX = 0F;
+					oldY = 0F;
+					break;
+
+				default:
+					break;
+				}
 				return false;
 			}
 		});
@@ -138,28 +130,28 @@ public class ClassifyFragment extends BaseFragment {
 		ULog.d("v.id = " + v.getId());
 		switch (v.getId()) {
 		case R.id.app_top_title_iv_left:
-			/*Intent intent = new Intent();
-			intent.setClass(getActivity(), MCaptureActivity.class);
-			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			startActivityForResult(intent, 1);
-			break;*/
+			/*
+			 * Intent intent = new Intent(); intent.setClass(getActivity(), MCaptureActivity.class);
+			 * intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); startActivityForResult(intent, 1); break;
+			 */
 			startActivity(new Intent(getActivity(), SearchAcitivity.class));
 			break;
 		default:
 			break;
 		}
 	}
+
 	/**
 	 * 
 	 * @return 右边的Listview是否为打开状态
 	 */
-	public boolean getRightLinearlayoutIsShow(){
+	public boolean getRightLinearlayoutIsShow() {
 		int visible = lin_content_right.getVisibility();
-		
-		if (visible ==View.VISIBLE) 
+
+		if (visible == View.VISIBLE)
 			return true;
 		else
 			return false;
-		
+
 	}
 }
---
+++ trunk/ my-com-android/Lottery_ZT/src/com/next/lottery/fragment/UserHelpCenterFragment.java
@@ -0,0 +1,55 @@
+package com.next.lottery.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dongfang.v4.app.BaseFragment;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.event.OnClick;
+import com.next.lottery.R;
+import com.next.lottery.view.SlidingMenu;
+
+/**
+ * 帮助中心
+ * 
+ * @author dongfang
+ * 
+ */
+
+public class UserHelpCenterFragment extends BaseFragment {
+	private SlidingMenu mSlidingMenu;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.activity_user_helpcenter, container, false);
+		ViewUtils.inject(this, view);
+		return view;
+	}
+
+	public SlidingMenu getmSlidingMenu() {
+		return mSlidingMenu;
+	}
+
+	public void setmSlidingMenu(SlidingMenu mSlidingMenu) {
+		this.mSlidingMenu = mSlidingMenu;
+	}
+
+	@Override
+	@OnClick({ R.id.app_top_title_iv_left, R.id.app_top_title_iv_rigth })
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.app_top_title_iv_left:
+			getActivity().finish();
+			break;
+		case R.id.app_top_title_iv_rigth:
+			if (null != mSlidingMenu)
+				mSlidingMenu.showRightView();
+			break;
+
+		default:
+			break;
+		}
+	}
+}
