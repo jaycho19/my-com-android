6eff77fe288f9efec2969190558b60f566425320
dongfanglinglong <dongfanglinglong@gmail.com>
2014-05-05 09:07:29 +0000

    修改我的订单title显示

---  my-com-android/Lottery_ZT/src/com/next/lottery/MyOrderListActivity.java
+++  my-com-android/Lottery_ZT/src/com/next/lottery/MyOrderListActivity.java
@@ -1,16 +1,8 @@
 package com.next.lottery;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
-import android.view.View;
 
-import com.dongfang.v4.app.BaseActivity;
-import com.lidroid.xutils.ViewUtils;
-import com.lidroid.xutils.view.annotation.ViewInject;
 import com.next.lottery.fragment.MyOrderListFragment;
-import com.next.lottery.fragment.RightMenuFragment;
-import com.next.lottery.fragment.UserSettingFragment;
-import com.next.lottery.view.SlidingMenu;
 
 /**
  * 我的订单
@@ -24,5 +16,6 @@ public class MyOrderListActivity extends BaseSlidingMenuActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setCenterFragment(new MyOrderListFragment());
+		setTopTitle("我的订单");
 	}
 }
