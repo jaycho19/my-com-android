8499450ec85848e9586c46ae583273bfb3bf704a
dongfanglinglong@gmail.com <dongfanglinglong@gmail.com@afd60443-85ab-dd9c-788a-5eb6f25d9bf9>
2014-05-20 09:06:25 +0000

    增加用户修改界面

--- trunk/APK/Daohang/src/com/dongfang/daohang/fragment/SettingFragment.java
+++ trunk/APK/Daohang/src/com/dongfang/daohang/fragment/SettingFragment.java
@@ -1,15 +1,20 @@
 package com.dongfang.daohang.fragment;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.dongfang.daohang.R;
 import com.dongfang.daohang.SettingAboutActivity;
 import com.dongfang.daohang.SettingFeedbackActivity;
 import com.dongfang.daohang.SettingMapDownloadActivity;
+import com.dongfang.daohang.dialog.MMAlert;
+import com.dongfang.daohang.dialog.MMAlert.OnAlertSelectId;
 import com.dongfang.v4.app.BaseFragment;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.event.OnClick;
@@ -40,10 +45,21 @@ public class SettingFragment extends BaseFragment {
 			getActivity().startActivity(new Intent(getActivity(), SettingMapDownloadActivity.class));
 			break;
 		case R.id.fragment_setting_tv_invite: // 邀请好友
+			MMAlert.showAlert(getActivity(), null, new String[] { "微信", "新浪微博", "微信朋友圈" }, null, new OnAlertSelectId() {
+				@Override
+				public void onClick(int whichButton) {}
+			});
 			break;
 		case R.id.fragment_setting_tv_evaluate: // 评论
+			try{
+			    Uri uri = Uri.parse("market://details?id="+getActivity().getPackageName());  
+			    Intent intent = new Intent(Intent.ACTION_VIEW,uri);  
+			    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);          
+			    startActivity(intent);
+			}catch(ActivityNotFoundException e){
+			    Toast.makeText(getActivity(), "Couldn't launch the market !", Toast.LENGTH_SHORT).show();
+			}
 			break;
-
 		default:
 			break;
 		}
