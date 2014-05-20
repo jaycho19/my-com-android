bebc990df5726ee82dedfde246fff796a0c1ca9f
dongfanglinglong <dongfanglinglong@gmail.com>
2014-05-20 04:07:55 +0000

    增加底部弹出框

--- APK/Daohang/src/com/dongfang/daohang/UserInfoActivity.java
+++ APK/Daohang/src/com/dongfang/daohang/UserInfoActivity.java
@@ -2,8 +2,13 @@ package com.dongfang.daohang;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
+import com.dongfang.daohang.dialog.MMAlert;
+import com.dongfang.daohang.dialog.MMAlert.OnAlertSelectId;
 import com.dongfang.v4.app.BaseActivity;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.lidroid.xutils.view.annotation.event.OnClick;
 
 /**
  * 关注
@@ -13,13 +18,34 @@ import com.dongfang.v4.app.BaseActivity;
  */
 public class UserInfoActivity extends BaseActivity {
 
+	@ViewInject(R.id.activity_user_info_rl_header)
+	private RelativeLayout rlHeader;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_user_info);
 	}
 
+	@OnClick({ R.id.activity_user_info_rl_header })
 	@Override
-	public void onClick(View v) {}
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.activity_user_info_rl_header:
+			MMAlert.showAlert(this, "2222222", new String[]{"11111","22222222","333333333","4444444444"}, "退出",new OnAlertSelectId(){
+
+				@Override
+				public void onClick(int whichButton) {
+					
+				}
+				
+			} );
+			break;
+
+		default:
+			break;
+		}
+		
+	}
 
 }
