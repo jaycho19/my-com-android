59f155e70924a1463cf8a0cb7eeccefc6ac4422b
dongfanglinglong@gmail.com <dongfanglinglong@gmail.com@afd60443-85ab-dd9c-788a-5eb6f25d9bf9>
2014-06-11 14:32:27 +0000

    修改注册流程中的bug； 修改楼层页面增加的对话框；

--- trunk/APK/Daohang/src/com/dongfang/daohang/dialog/RecordDialog.java
+++ trunk/APK/Daohang/src/com/dongfang/daohang/dialog/RecordDialog.java
@@ -7,12 +7,13 @@ import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.dongfang.daohang.R;
+import com.dongfang.daohang.beans.AreaBean;
 import com.dongfang.daohang.beans.BaseEntity;
 import com.dongfang.daohang.net.HttpActions;
-import com.dongfang.dialog.ProgressDialog;
 import com.dongfang.utils.DFException;
 import com.dongfang.utils.JsonAnalytic;
 import com.dongfang.utils.ULog;
@@ -36,7 +37,9 @@ public class RecordDialog {
 		LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.dialog_record, null);
 		dlg.setContentView(layout);
 
-		OnRecordListener listener = new OnRecordListener(context, dlg, str);
+		ProgressBar pb = (ProgressBar) layout.findViewById(R.id.dialog_record_progressbar);
+
+		OnRecordListener listener = new OnRecordListener(context, dlg, str, pb);
 
 		layout.findViewById(R.id.dialog_record_cancel).setOnClickListener(listener);
 		layout.findViewById(R.id.dialog_record_recorded).setOnClickListener(listener);
@@ -50,11 +53,13 @@ public class RecordDialog {
 		private Dialog dialog;
 		private String str;
 		private Context context;
+		private ProgressBar pb;
 
-		public OnRecordListener(final Context context, Dialog dialog, String str) {
+		public OnRecordListener(final Context context, Dialog dialog, String str, ProgressBar pb) {
 			this.dialog = dialog;
 			this.str = str;
 			this.context = context;
+			this.pb = pb;
 		}
 
 		@Override
@@ -64,39 +69,40 @@ public class RecordDialog {
 				dialog.dismiss();
 				break;
 			case R.id.dialog_record_recorded: {
-				int placeId = 2;
-				String areaId = "A1";
-				new HttpUtils().send(HttpMethod.GET, HttpActions.addRecord(context, areaId, placeId),
+				ULog.d(str);
+				AreaBean bean = null;
+				try {
+					bean = JsonAnalytic.getInstance().analyseJsonT(str, AreaBean.class);
+				} catch (DFException e) {
+					ULog.e(e.getMessage());
+				}
+
+				if (null == bean)
+					return;
+
+				pb.setVisibility(View.VISIBLE);
+
+				new HttpUtils().send(HttpMethod.GET, HttpActions.addRecord(context, bean.getAreaId(), 10),
 						new RequestCallBack<String>() {
-							ProgressDialog pro = ProgressDialog.show(context);
-
-							@Override
-							public void onStart() {
-								pro.show();
-							}
-
 							@Override
 							public void onFailure(HttpException arg0, String arg1) {
-								pro.dismiss();
-								//Toast.makeText(context, "添加记录点失败〒_〒", Toast.LENGTH_SHORT).show();
-								ULog.d("添加记录点失败〒_〒");
+								Toast.makeText(context, "添加记录点失败〒_〒", Toast.LENGTH_SHORT).show();
 								dialog.dismiss();
 							}
 
 							@Override
 							public void onSuccess(ResponseInfo<String> arg0) {
-								pro.dismiss();
 								ULog.d(arg0.result);
 								try {
 									BaseEntity base = JsonAnalytic.getInstance().analyseJsonTDF(arg0.result,
 											BaseEntity.class);
 									ULog.d(base.getMsg());
 
-									//Toast.makeText(context, base.getMsg(), Toast.LENGTH_SHORT).show();
+									Toast.makeText(context, base.getMsg(), Toast.LENGTH_SHORT).show();
 								} catch (DFException e) {
 									ULog.d(e.getMessage());
 
-									//Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+									Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
 								}
 
 								dialog.dismiss();
